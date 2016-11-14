b7b32989bad776c6563681a85b04b478bec59eed
Titus Wormer <tituswormer@gmail.com>
2016-11-14 12:00:34 +0100

    Refactor code-style

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module meta-name
- * @fileoverview List of values for `name` on `meta`.
- */
-
 'use strict';
-
-/* eslint-env commonjs */
-
-/*
- * Expose.
- */
-
 module.exports = require('./index.json');
--- test.js
+++ test.js
@@ -1,39 +1,23 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module meta-name
- * @fileoverview Test suite for `meta-name`.
- */
-
 'use strict';
 
-/* eslint-env node */
-
-/*
- * Module dependencies.
- */
-
+var assert = require('assert');
 var test = require('tape');
 var metaName = require('./index.js');
 
-/*
- * Tests.
- */
-
 test('metaName', function (t) {
-    t.ok(
-        Array.isArray(metaName),
-        'should be an `array`'
-    );
+  t.ok(
+    Array.isArray(metaName),
+    'should be an `array`'
+  );
 
-    metaName.forEach(function (tagName) {
-        t.equal(
-            typeof tagName,
-            'string',
-            '`' + tagName + '` should be a string'
-        );
-    });
+  t.doesNotThrow(
+    function () {
+      metaName.forEach(function (tagName) {
+        assert.equal(typeof tagName, 'string', tagName);
+      });
+    },
+    'should be all `string`s'
+  );
 
-    t.end();
+  t.end();
 });

27a50bd86032d1231530a7b1ca283178964f3f24
Titus Wormer <tituswormer@gmail.com>
2017-06-25 21:51:00 +0200

    Refactor build script

--- build.js
+++ build.js
@@ -1,23 +1,34 @@
 'use strict';
 
 var fs = require('fs');
+var path = require('path');
 var https = require('https');
-var concat = require('concat-stream');
 var bail = require('bail');
+var concat = require('concat-stream');
+var unified = require('unified');
+var parse = require('rehype-parse');
+var q = require('hast-util-select');
+var toString = require('hast-util-to-string');
+var list = require('./');
 
-https.get('https://help.whatwg.org/extensions/meta-name/', function (res) {
-  res
-    .pipe(concat(function (buf) {
-      var value = String(buf)
-        .split('\n')
-        .sort()
-        .filter(Boolean);
+https.get('https://wiki.whatwg.org/wiki/MetaExtensions', function (res) {
+  res.pipe(concat(onconcat)).on('error', bail);
 
-      fs.writeFile(
-        'index.json',
-        JSON.stringify(value, 0, 2) + '\n',
-        bail
-      );
-    }))
-    .on('error', bail);
+  function onconcat(buf) {
+    var table = q.select('table', unified().use(parse).parse(buf));
+
+    q.selectAll('tbody tr td:first-child', table).forEach(function (node) {
+      var data = toString(node).trim().toLowerCase();
+
+      if (data && list.indexOf(data) === -1) {
+        list.push(data);
+      }
+    });
+
+    fs.writeFile(
+      path.join(__dirname, 'index.json'),
+      JSON.stringify(list.sort(), 0, 2) + '\n',
+      bail
+    );
+  }
 });

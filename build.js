295365d95f7351de2f190b0d8e68ac79def94802
Titus Wormer <tituswormer@gmail.com>
2022-11-16 10:27:16 +0100

    Refactor build script

--- build.js
+++ build.js
@@ -1,47 +1,40 @@
-import assert from 'node:assert'
-import https from 'node:https'
-import fs from 'node:fs'
-import {bail} from 'bail'
-import concatStream from 'concat-stream'
-import {unified} from 'unified'
-import rehypeParse from 'rehype-parse'
+import assert from 'node:assert/strict'
+import fs from 'node:fs/promises'
+import fetch from 'node-fetch'
+import {fromHtml} from 'hast-util-from-html'
 import {select, selectAll} from 'hast-util-select'
 import {toString} from 'hast-util-to-string'
 import {metaName} from './index.js'
 
-https.get('https://wiki.whatwg.org/wiki/MetaExtensions', (response) => {
-  response.pipe(concatStream(onconcat)).on('error', bail)
-})
+const response = await fetch('https://wiki.whatwg.org/wiki/MetaExtensions')
+const text = await response.text()
+const tree = fromHtml(text)
 
-/**
- * @param {Buffer} buf
- */
-function onconcat(buf) {
-  const table = select('table', unified().use(rehypeParse).parse(buf))
-  assert(table, 'expected `table`')
-  const cells = selectAll('tbody tr td:first-child', table)
-  let index = -1
+const table = select('table', tree)
+assert(table, 'expected `table`')
+const cells = selectAll('tbody tr td:first-child', table)
+let index = -1
 
-  while (++index < cells.length) {
-    const data = toString(cells[index]).trim().toLowerCase()
+while (++index < cells.length) {
+  const data = toString(cells[index]).trim().toLowerCase()
 
-    if (data && !metaName.includes(data)) {
-      metaName.push(data)
-    }
+  if (data) {
+    metaName.push(data)
   }
-
-  fs.writeFile(
-    'index.js',
-    [
-      '/**',
-      ' * List of values that can be used as `name`s on HTML `meta` elements',
-      ' *',
-      ' * @type {string[]}',
-      ' */',
-      'export const metaName = ',
-      JSON.stringify(metaName.sort(), null, 2),
-      ''
-    ].join('\n'),
-    bail
-  )
 }
+
+const list = [...new Set(metaName)].sort()
+
+await fs.writeFile(
+  'index.js',
+  [
+    '/**',
+    ' * List of values that can be used as `name`s on HTML `meta` elements',
+    ' *',
+    ' * @type {Array<string>}',
+    ' */',
+    'export const metaName = ',
+    JSON.stringify(list, null, 2),
+    ''
+  ].join('\n')
+)
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * List of values that can be used as `name`s on HTML `meta` elements
  *
- * @type {string[]}
+ * @type {Array<string>}
  */
 export const metaName = [
   'aglsterms.accessibility',
